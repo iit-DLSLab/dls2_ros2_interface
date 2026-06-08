b27e8216403e392d8bf109df72d10ef4aa890d28
MMarcus95 <marco.marchitto@iit.it>
2026-06-08 18:22:14 +0200

    cleanup

--- examples/pid_ws/build/pid/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/iit.local/mmarchitto/dls2_ros2_bridge/examples/pid_ws/src/pid
-# Build directory: /home/iit.local/mmarchitto/dls2_ros2_bridge/examples/pid_ws/build/pid
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
